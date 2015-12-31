24a72cb5027b1943b662f5bb904d86551aad77be
raaghulrdeveloper <raaghulrdev@gmail.com>
2015-12-31 15:45:00 +0530

    Correct Second Page Intent from First

--- app/src/main/java/com/project/money/Second.java
+++ app/src/main/java/com/project/money/Second.java
@@ -1,7 +1,25 @@
 package com.project.money;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
 /**
  * Created by raaghulr on 30-12-2015.
  */
-public class Second {
+public class Second extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+
+    }
 }
+
+
+
