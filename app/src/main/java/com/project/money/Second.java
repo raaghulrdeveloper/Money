c21336a02e6f919451a4e3c2100c55a3629d7d51
raaghulrdeveloper <raaghulrdev@gmail.com>
2016-01-03 04:27:39 +0530

    Direct Push From Android Studio

--- app/src/main/java/com/project/money/Second.java
+++ app/src/main/java/com/project/money/Second.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by raaghulr on 30-12-2015.
@@ -18,6 +19,12 @@ public class Second extends AppCompatActivity {
 
 
 
+    }
+
+    public void myfunc(View v)
+    {
+        Toast toast = Toast.makeText(getApplicationContext(), "you clicked the method",Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
 
