f3bab67b270526d5130dadf5d517c5c5a996d150
raaghulrdeveloper <raaghulrdev@gmail.com>
2016-01-03 22:10:11 +0530

    attempt menu

--- app/src/main/java/com/project/money/Second.java
+++ app/src/main/java/com/project/money/Second.java
@@ -1,15 +1,11 @@
 package com.project.money;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import com.project.money.Menu;
 
 /**
  * Created by raaghulr on 30-12-2015.
@@ -26,7 +22,7 @@ public class Second extends AppCompatActivity {
 
     public void myfunc(View v)
     {
-        Toast toast = Toast.makeText(getApplicationContext(), "you clicked the method",Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(getApplicationContext(), "you clicked the fab button!!!",Toast.LENGTH_SHORT);
         toast.show();
     }
 
@@ -34,20 +30,7 @@ public class Second extends AppCompatActivity {
 
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
 
-
-
-    public void click(MenuItem i)
-    {
-
-        Toast.makeText(this, "you clicked the menu", Toast.LENGTH_SHORT).show();
-
-    }
 
 
 }
