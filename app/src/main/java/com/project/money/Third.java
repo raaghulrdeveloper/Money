e06d3893cda417f83d5080476148fdc5d44e717b
raaghulrdeveloper <raaghulrdev@gmail.com>
2016-01-03 20:46:09 +0530

    this is third file

---
+++ app/src/main/java/com/project/money/Third.java
@@ -0,0 +1,17 @@
+package com.project.money;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by raagh_000 on 1/3/2016.
+ */
+public class Third extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+
+    }
+}
