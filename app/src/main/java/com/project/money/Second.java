2cb3df4ae91dab3790fa0f0fdf1e87a9350f56a0
raaghulrdeveloper <raaghulrdev@gmail.com>
2016-01-04 12:37:34 +0530

    background for second page

--- app/src/main/java/com/project/money/Second.java
+++ app/src/main/java/com/project/money/Second.java
@@ -1,10 +1,15 @@
 package com.project.money;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 /**
@@ -15,6 +20,13 @@ public class Second extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        Bitmap bmp = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.bg),size.x,size.y,true);
+        ImageView iv_background = (ImageView) findViewById(R.id.img_bg2);
+        iv_background.setImageBitmap(bmp);
 
 
 
